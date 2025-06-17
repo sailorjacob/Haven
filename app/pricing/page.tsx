5243c9603f94d5de0353ae3d0a471bdcf0d5614e
sailorjacob <x2sides@gmail.com>
2025-06-17 19:21:33 -0400

    Lighten pricing page gear opacity by half

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -71,7 +71,7 @@ export default function PricingPage() {
         {/* Spinning background gear */}
         <motion.svg
           viewBox="0 0 32 32"
-          className="pointer-events-none absolute -top-40 -left-40 w-[500px] h-[500px] text-zinc-300/40"
+          className="pointer-events-none absolute -top-40 -left-40 w-[500px] h-[500px] text-zinc-200/20"
           initial={{ rotate: 0 }}
           animate={{ rotate: 360 }}
           transition={{ duration: 120, repeat: Infinity, ease: "linear" }}
