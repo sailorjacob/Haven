fa9f111c75e806175d4bbe6eec2cf45f1249f9e3
sailorjacob <x2sides@gmail.com>
2025-12-25 20:49:09 -0400

    Replace designforstartups image with new design for startups image

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function HomePage() {
       title: "designforstartups.net",
       description: "branding and design for new and special projects.",
       technologies: ["Branding", "Design", "Identity"],
-      imageUrl: "https://twejikjgxkzmphocbvpt.supabase.co/storage/v1/object/public/havensvgs/designforstartup.png",
+      imageUrl: "https://twejikjgxkzmphocbvpt.supabase.co/storage/v1/object/public/havensvgs/design%20for%20startups.png",
       link: "https://designforstartups.net",
       category: "website"
     },
